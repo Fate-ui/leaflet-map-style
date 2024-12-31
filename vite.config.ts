ebd375a5357e9fa6a784c01e8f4a43be4505f01c
wy <228574227@qq.com>
2024-12-31 16:17:35 +0800

    ✨feat(全局)：初次提交

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,17 @@
-/// <reference types="vitest" />
 import path from 'path'
 import { defineConfig } from 'vite'
 import { createVitePlugins } from './config/vitePlugin'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  test: {
-    //单元测试
-    environment: 'happy-dom',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '#': path.resolve(__dirname, 'types'),
-    },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-  },
-  esbuild: {
-    pure: ['console.log'], // 删除 console.log
-  },
-  plugins: createVitePlugins(),
-  server: {
-    host: '0.0.0.0',
-    hmr: true,
-    port: 9527,
-    open: false,
-    proxy: {
-      '/api/v1': {
-        // target: 'http://10.10.10.123:8874', // 内网
-        // target: 'https://aba-data.vankeytech.com:9902/', // 测试环境
-        target: 'http://192.168.0.47:8080', // 谢梁本地
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const base = mode === 'development' ? '/' : '/leaflet-map-style'
+  return {
+    base,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
     },
-  },
+    plugins: createVitePlugins(),
+  }
 })
